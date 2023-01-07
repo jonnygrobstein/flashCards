bfbc9ce249a77f7e5401a9bba0566deb491f9666
Jonny Grobstein <jonnygrobstein@gmail.com>
2023-01-07 13:50:06 -0600

    more JS done. randomly picks from the key value pairs

--- flashcards.js
+++ flashcards.js
@@ -1,6 +1,7 @@
 
 
 const term = document.querySelector('.term')
+const definition = document.querySelector('.cardDefinition')
 const previousButton = document.querySelector('.previousCard')
 const flipButton = document.querySelector('.flipCard')
 const nextButton = document.querySelector('.nextCard')
@@ -10,7 +11,7 @@ previousButton.addEventListener('click', function() {
 })
 
 flipButton.addEventListener('click', function() {
-    console.log('You clicked the Flip Card Button')
+    definition.style.display = 'block';
 })
 
 nextButton.addEventListener('click', function() {
@@ -43,8 +44,23 @@ addEventListener
 
 
 
-// cards = {
-//     'Which is the only American Football team to go a whole season undefeated, including the Super Bowl?': '1972 Miami Dolphins',
-//     'Which is the only team to play in every soccer World Cup tournament?': Brazil,
-//     'At the 1976 Olympic Games in Montreal, gymnast Nadia Comaneci became the first gymnast to score a perfect 10. Which country was she representing?'
-// }
+let cards = {
+    'Which is the only American Football team to go a whole season undefeated, including the Super Bowl?': '1972 Miami Dolphins',
+    'Which is the only team to play in every soccer World Cup tournament?': 'Brazil',
+    'At the 1976 Olympic Games in Montreal, gymnast Nadia Comaneci became the first gymnast to score a perfect 10. Which country was she representing?': 'Romania',
+    'Which American Football team won the first two Super Bowls in 1967 and 1968': 'Green Bay Packers',
+    'Which two racing drivers hold the record for the most Formula One World Drivers Championship wins, with seven titles?': 'Michael Schumacher and Lewis Hamilton',
+    'Which NFL team appeared in four consecutive Super Bowls between 1991 and 1994 and lost them all?': 'Buffalo Bills',
+    'Which boxer was the first to defeat Muhammad Ali in professional boxing?': 'Joe Frazier, 1971',
+
+}
+
+data = Object.entries(cards)
+
+console.log(data[0][0])
+
+function getRandomTerm() {
+    randomTerm = data[Math.floor(Math.random() * data.length)]
+    term.innerHTML = ``
+}
+
