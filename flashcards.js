3baa8a4c783b1171e99c34bfbe02f16c9b6b25a1
Jonny Grobstein <jonnygrobstein@gmail.com>
2023-01-07 13:18:12 -0600

    first commit.  HTML and CSS good for now.  Starting on JS.

---
+++ flashcards.js
@@ -0,0 +1,50 @@
+
+
+const term = document.querySelector('.term')
+const previousButton = document.querySelector('.previousCard')
+const flipButton = document.querySelector('.flipCard')
+const nextButton = document.querySelector('.nextCard')
+
+previousButton.addEventListener('click', function() {
+    console.log('You clicked the Previous Card Button')
+})
+
+flipButton.addEventListener('click', function() {
+    console.log('You clicked the Flip Card Button')
+})
+
+nextButton.addEventListener('click', function() {
+    console.log('You clicked the Next Card Button')
+})
+
+
+
+questions = []
+answers = []
+
+
+
+// Hide both #term and #answer from view
+
+
+
+
+
+// show card with term in the card and not answer - click default to start the first term
+
+addEventListener
+
+// click the showAnswer button and transition card to answerCard
+
+addEventListener
+
+
+// click the #nextCard button and transition card to next card
+
+
+
+// cards = {
+//     'Which is the only American Football team to go a whole season undefeated, including the Super Bowl?': '1972 Miami Dolphins',
+//     'Which is the only team to play in every soccer World Cup tournament?': Brazil,
+//     'At the 1976 Olympic Games in Montreal, gymnast Nadia Comaneci became the first gymnast to score a perfect 10. Which country was she representing?'
+// }
